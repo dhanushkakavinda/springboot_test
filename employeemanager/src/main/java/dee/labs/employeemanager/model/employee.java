ff08cd24a9a0dde7696145ee4be5cdaddc4e62c1
dhanushkakavinda <76260998+dhanushkakavinda@users.noreply.github.com>
2021-05-04 13:36:28 +0530

    Add files via upload

---
+++ employeemanager/src/main/java/dee/labs/employeemanager/model/employee.java
@@ -0,0 +1,11 @@
+package dee.labs.employeemanager.model;
+
+public class employee {
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+    private String employeeCode;
+}
